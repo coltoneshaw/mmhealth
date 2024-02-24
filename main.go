7a34565c2be747e7327367bb82775cdcfc38c8a6
coltoneshaw <colton@theshaw.group>
2024-02-24 11:07:53 -0500

    removed bad embed

--- main.go
+++ main.go
@@ -4,13 +4,8 @@ Copyright © 2024 Colton Shaw
 package main
 
 import (
-	"embed"
-
 	"github.com/coltoneshaw/mmhealth/mmhealth/cmd"
 )
-
-//go:embed checks.yaml config.yaml
-var f embed.FS
 
 func main() {
 	cmd.Execute()
